781f63022f25041d6160792955849e21d4ce1299
Zachary Christiansen <volfan6415@gmail.com>
2018-06-13 13:56:57 -0700

    new file:   .classpath 	new file:   .gitignore 	new file:   .project 	new file:   ESH-INF/binding/binding.xml 	new file:   ESH-INF/i18n/konnected_xx_XX.properties 	new file:   ESH-INF/thing/thing-types.xml 	new file:   META-INF/MANIFEST.MF 	new file:   OSGI-INF/.gitignore 	new file:   README.md 	new file:   about.html 	new file:   build.properties 	new file:   pom.xml 	new file:   src/main/java/org/openhab/binding/konnected/internal/Discovery/KonnectedUPnPServer.java 	new file:   src/main/java/org/openhab/binding/konnected/internal/KonnectedBindingConstants.java 	new file:   src/main/java/org/openhab/binding/konnected/internal/KonnectedConfiguration.java 	new file:   src/main/java/org/openhab/binding/konnected/internal/KonnectedHandler.java 	new file:   src/main/java/org/openhab/binding/konnected/internal/KonnectedHandlerFactory.java 	new file:   target/classes/org/openhab/binding/konnected/internal/Discovery/KonnectedUPnPServer.class 	new file:   target/classes/org/openhab/binding/konnected/internal/KonnectedBindingConstants.class 	new file:   target/classes/org/openhab/binding/konnected/internal/KonnectedConfiguration.class 	new file:   target/classes/org/openhab/binding/konnected/internal/KonnectedHandler.class 	new file:   target/classes/org/openhab/binding/konnected/internal/KonnectedHandlerFactory.class

---
+++ src/main/java/org/openhab/binding/konnected/internal/KonnectedBindingConstants.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2014,2018 by the respective copyright holders.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.konnected.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link KonnectedBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Zachary Christiansen - Initial contribution
+ */
+@NonNullByDefault
+public class KonnectedBindingConstants {
+
+    private static final String BINDING_ID = "konnected";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_MODULE = new ThingTypeUID(BINDING_ID, "module");
+
+    // List of all Channel ids
+    public static final String Zone_1 = "zone1";
+
+    // Bridge config properties
+    public static final String HOST = "ipAddress";
+    public static final String PORT = "port";
+    public static final String MAC_ADDR = "macAddress";
+
+}
---
+++ src/main/java/org/openhab/binding/konnected/internal/KonnectedConfiguration.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2014,2018 by the respective copyright holders.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.konnected.internal;
+
+/**
+ * The {@link KonnectedConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Zachary Christiansen - Initial contribution
+ */
+public class KonnectedConfiguration {
+
+    /**
+     * Sample configuration parameter. Replace with your own.
+     */
+    public String config1;
+    public String HOST;
+}
